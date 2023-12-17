a9d4d91456d79b223352b60c627c4c0721e0538c
Sabbir Ahmed <39632780+Sabbirmadman@users.noreply.github.com>
2023-12-17 19:02:47 +0600

    initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Drawer, Button } from "antd";
+import Navbar from "./components/menu/Navbar";
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+            <h1>Hello Vite + React!</h1>
+        </div>
+    );
+}
+
+export default App;
