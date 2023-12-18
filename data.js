7e857583dee71114d26cffb7a9aca5bc110c68ec
Sabbir Ahmed <39632780+Sabbirmadman@users.noreply.github.com>
2023-12-18 06:35:04 +0600

    ok

---
+++ data.js
@@ -0,0 +1,51 @@
+const phoneData = [
+    {
+        id: 1,
+        type: "IPHONE",
+        model: "iPhone 13 Pro Max",
+        image: "https://static.wixstatic.com/media/99b596_3db049ec2ff14d56bbfb3289815fd2ed~mv2.png/v1/fill/w_334,h_395,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Image-empty-state_edited.png",
+    },
+    {
+        id: 2,
+        type: "IPHONE",
+        model: "iPhone 13 Pro",
+        image: "https://static.wixstatic.com/media/99b596_0837055b6d344fb6aa4a60cac8533a30~mv2.png/v1/fill/w_334,h_395,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Image-empty-state_edited.png",
+    },
+    {
+        id: 3,
+        type: "IPHONE",
+        model: "iPhone 12 Pro Max",
+        image: "https://static.wixstatic.com/media/99b596_154bfd97bc5f4c81934bb259fa66a5f8~mv2.png/v1/fill/w_334,h_395,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Image-empty-state_edited.png",
+    },
+    {
+        id: 4,
+        type: "IPHONE",
+        model: "iPhone 13 ",
+        image: "https://static.wixstatic.com/media/99b596_6f0e8b1b02244373ab6e4ccc26170d76~mv2.png/v1/fill/w_334,h_395,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Image-empty-state_edited.png",
+    },
+];
+
+const catagoryTypes = [
+    {
+        id: 1,
+        name: "iPhone",
+        image: "https://static.wixstatic.com/media/99b596_3db049ec2ff14d56bbfb3289815fd2ed~mv2.png/v1/fill/w_334,h_395,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Image-empty-state_edited.png",
+    },
+    {
+        id: 2,
+        name: "Samsung",
+        image: "https://static.wixstatic.com/media/99b596_0837055b6d344fb6aa4a60cac8533a30~mv2.png/v1/fill/w_334,h_395,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Image-empty-state_edited.png",
+    },
+    {
+        id: 3,
+        name: "Google",
+        image: "https://static.wixstatic.com/media/99b596_154bfd97bc5f4c81934bb259fa66a5f8~mv2.png/v1/fill/w_334,h_395,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Image-empty-state_edited.png",
+    },
+    {
+        id: 4,
+        name: "OnePlus",
+        image: "https://static.wixstatic.com/media/99b596_6f0e8b1b02244373ab6e4ccc26170d76~mv2.png/v1/fill/w_334,h_395,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Image-empty-state_edited.png",
+    },
+];
+
+export { phoneData, catagoryTypes };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import CarouselComponent from "../components/carousel/CarouselComponent";
 
 export default function HomePage() {
-    return <div>HomePage</div>;
+    return (
+        <div>
+            <CarouselComponent />
+        </div>
+    );
 }
