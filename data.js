c3ead4b3dfe07b2c14f276c904d489336f76ac40
Sabbir Ahmed <39632780+Sabbirmadman@users.noreply.github.com>
2023-12-18 23:33:19 +0600

    wadad

--- data.js
+++ data.js
@@ -29,22 +29,26 @@ const catagoryTypes = [
     {
         id: 1,
         name: "iPhone",
-        image: "https://static.wixstatic.com/media/99b596_3db049ec2ff14d56bbfb3289815fd2ed~mv2.png/v1/fill/w_334,h_395,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Image-empty-state_edited.png",
+        image: "https://static.wixstatic.com/media/99b596_02c7d183c9f846d482eff23d71fb049f~mv2.png/v1/fill/w_128,h_174,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/iPhone-12-Procracked-screen.png",
+        bg: "#f5c5b0",
     },
     {
         id: 2,
         name: "Samsung",
-        image: "https://static.wixstatic.com/media/99b596_0837055b6d344fb6aa4a60cac8533a30~mv2.png/v1/fill/w_334,h_395,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Image-empty-state_edited.png",
+        image: "https://static.wixstatic.com/media/99b596_5cb4c79e756c447caffd720561df6fbf~mv2.png/v1/fill/w_200,h_183,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/1144404-middle.png",
+        bg: "#f5b6b0",
     },
     {
         id: 3,
         name: "Google",
-        image: "https://static.wixstatic.com/media/99b596_154bfd97bc5f4c81934bb259fa66a5f8~mv2.png/v1/fill/w_334,h_395,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Image-empty-state_edited.png",
+        image: "https://static.wixstatic.com/media/99b596_3a11c99684034409af6b0703ac99a5e0~mv2.png/v1/fill/w_196,h_192,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/tab.png",
+        bg: "#d9c1b4",
     },
     {
         id: 4,
         name: "OnePlus",
-        image: "https://static.wixstatic.com/media/99b596_6f0e8b1b02244373ab6e4ccc26170d76~mv2.png/v1/fill/w_334,h_395,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Image-empty-state_edited.png",
+        image: "https://static.wixstatic.com/media/99b596_95ab285494c74157ba977be5ef29f09e~mv2.png/v1/fill/w_226,h_151,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/laptop.png",
+        bg: "#dbd1b6",
     },
 ];
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import CarouselComponent from "../components/carousel/CarouselComponent";
+import FixListComponent from "../components/FixListComponent";
+import AboutComponent from "../components/AboutComponent";
 
 export default function HomePage() {
     return (
         <div>
             <CarouselComponent />
+            <FixListComponent />
+            <AboutComponent />
         </div>
     );
 }
